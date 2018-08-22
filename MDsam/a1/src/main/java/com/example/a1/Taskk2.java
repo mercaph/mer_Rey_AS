3a9d78426683d4d874ff273127367930f26ce284
mercaph <1095951873@qq.cn>
2018-08-22 13:18:23 +0800

    13:18

---
+++ MDsam/a1/src/main/java/com/example/a1/Taskk2.java
@@ -0,0 +1,19 @@
+package com.example.a1;
+
+public class Taskk2 {
+    private String task_w;
+    private String time_w;
+
+
+    public Taskk2(String task_w,String time_w) {
+        this.task_w = task_w;
+        this.time_w = time_w;
+    }
+
+    public String getTask_w() {
+        return task_w;
+    }
+    public String getTime_w(){
+        return time_w;
+    }
+}
